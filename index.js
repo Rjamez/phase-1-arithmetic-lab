8313cec1566ef736380309887836a63ed5bc0e52
Rjamez <robin.adhola.student@moringaschool.com>
2024-10-08 08:45:10 +0300

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 let num1 = 31;
 let num2 = 2;
-const multiply= num1*num2;
+const multiply = num1*num2;
 
-const random =Math.ceil(Math.random() * 10) + 1;
+const random = Math.ceil(Math.random() * 10) + 1;
 
 
 let num3 = 13;
 let num4 = 30;
-const mod= num4 % num3;
+const mod = num4 % num3;
 
 
-const max =Math.max(10, 20, 8, 6, 7);
-// Write your code here
+const max = Math.max(10, 20, 8, 6, 7);
